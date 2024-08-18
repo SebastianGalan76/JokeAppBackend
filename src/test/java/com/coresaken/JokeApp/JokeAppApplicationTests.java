d5ef93cd3a2b11731d5fd778c5bead498e44366e
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-08-18 13:16:06 +0200

    Init project

---
+++ src/test/java/com/coresaken/JokeApp/JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coresaken.JokeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
